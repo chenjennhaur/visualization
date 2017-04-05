b9284dc371a8eb7040d6fd4f2f74ec0ca5362ad7
chenjennhaur <chenjennhaur@hotmail.com>
2017-04-05 17:52:09 +0800

    Update ggmap.R

--- ggmap.R
+++ ggmap.R
@@ -7,3 +7,7 @@ iata <- read_csv("GlobalAirportDatabase.csv")
 file2 <- read_csv("file.csv")
 iata_min <- select(iata,IATA,Lat,Long)
 orig_join <- inner_join(iata_min,file2,by=c("IATA"="Orig"))
+
+MX <- readOGR(dsn="MEX_adm_shp",layer="MEX_adm1")
+mxshp <- fortify(MX,region="HASC_1")
+ggplot(mxshp,aes(x=long,y=lat,group=group)) + geom_polygon(alpha=0.5,colour="black",size=0.2,fill=NA)
