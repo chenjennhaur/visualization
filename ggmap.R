de85221ccffaeca15fc9859d84aafebdce9a8057
chenjennhaur <chenjennhaur@hotmail.com>
2017-04-05 17:53:13 +0800

    Update ggmap.R

--- ggmap.R
+++ ggmap.R
@@ -11,3 +11,5 @@ orig_join <- inner_join(iata_min,file2,by=c("IATA"="Orig"))
 MX <- readOGR(dsn="MEX_adm_shp",layer="MEX_adm1")
 mxshp <- fortify(MX,region="HASC_1")
 ggplot(mxshp,aes(x=long,y=lat,group=group)) + geom_polygon(alpha=0.5,colour="black",size=0.2,fill=NA)
+
+# http://gis.stackexchange.com/questions/137621/join-spatial-point-data-to-polygons-in-r
